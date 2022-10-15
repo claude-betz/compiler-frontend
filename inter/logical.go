1d4d280270c3ac76de8267bd09c9138becf6dca7
Claude Betz <claude.betz47@gmail.com>
2022-10-15 15:17:13 +0200

    implement gen() for logical.go

--- inter/logical.go
+++ inter/logical.go
@@ -6,11 +6,16 @@
 
 package inter
 
-import "compiler-frontend/lexer"
+import (
+	"compiler-frontend/lexer"
+	"fmt"
+)
 
 type Logical struct {
+	typ   lexer.Type
 	expr1 Expr
 	expr2 Expr
+	Expr
 }
 
 func NewLogical(tok lexer.Token, expr1, expr2 Expr) Logical {
@@ -20,13 +25,24 @@ func NewLogical(tok lexer.Token, expr1, expr2 Expr) Logical {
 	}
 
 	return Logical{
+		typ:   typ,
 		expr1: expr1,
 		expr2: expr2,
 	}
 }
 
-func (l Logical) gen() {
-	// TODO
+func (l Logical) gen() Temp {
+	falseLine := newLabel()
+	after := newLabel()
+	temp := NewTemp(l.typ)
+
+	l.jumping(0, falseLine)
+	emit(fmt.Sprintf("%s = true", temp.toString()))
+	emit(fmt.Sprintf("goto L%d", after))
+	emitLabel(falseLine)
+	emit(fmt.Sprintf("%s = false", temp.toString()))
+	emitLabel(after)
+	return temp
 }
 
 func check(t1, t2 lexer.Type) lexer.Type {
